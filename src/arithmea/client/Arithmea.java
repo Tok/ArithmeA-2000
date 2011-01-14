22308301672933e862fb674086e85ecd549f0f30
Tok <tok9000@trash-mail.com>
2011-01-14 17:26:21 +0100

    added title image and center tags

--- src/arithmea/client/Arithmea.java
+++ src/arithmea/client/Arithmea.java
@@ -3,14 +3,18 @@ package arithmea.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Arithmea implements EntryPoint {
 
   public void onModuleLoad() {
-    ArithmeaServiceAsync rpcService = GWT.create(ArithmeaService.class);
+	ArithmeaServiceAsync rpcService = GWT.create(ArithmeaService.class);
     HandlerManager eventBus = new HandlerManager(null);
     AppController appViewer = new AppController(rpcService, eventBus);
-    appViewer.go(RootPanel.get());
+
+    RootPanel.get("title").add(new Image("/images/Title.png"));
+    
+    appViewer.go(RootPanel.get("content"));
   }
 }
